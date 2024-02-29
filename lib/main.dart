7482c537a611cf3918893a3f0d96daeba7c9a6aa
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-02-29 09:45:53 +0300

    Refactor HomePage to use StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,14 @@ void main()
   runApp(const HomePage());
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
