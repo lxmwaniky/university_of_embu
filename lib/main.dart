7dd29263c8d73396cbc0664a5313dc8f2aaefd52
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-03-03 01:29:16 +0300

    Add onTap functionality for menu items and bottom navigation items

--- lib/main.dart
+++ lib/main.dart
@@ -47,42 +47,72 @@ class HomePage extends StatelessWidget {
               ListTile(
                 title: Text('Home'),
                 leading: Icon(Icons.home),
+                onTap: () {
+                  // TODO: Implement onTap functionality for Home
+                },
               ),
               ListTile(
                 title: Text('Timetable'),
                 leading: Icon(Icons.calendar_today),
+                onTap: () {
+                  // TODO: Implement onTap functionality for Timetable
+                },
               ),              
               ListTile(
                 title: Text('Fees'),
                 leading: Icon(Icons.monetization_on),
+                onTap: () {
+                  // TODO: Implement onTap functionality for Fees
+                },
               ),
               ListTile(
                 title: Text('Reporting'),
                 leading: Icon(Icons.report),
+                onTap: () {
+                  // TODO: Implement onTap functionality for Reporting
+                },
               ),
               ListTile(
                 title: Text('Units'),
                 leading: Icon(Icons.book),
+                onTap: () {
+                  // TODO: Implement onTap functionality for Units
+                },
               ),
               ListTile(
                 title: Text('Examinations'),
                 leading: Icon(Icons.school),
+                onTap: () {
+                  // TODO: Implement onTap functionality for Examinations
+                },
               ),
               ListTile(
                 title: Text('Hostel Booking'),
                 leading: Icon(Icons.hotel),
+                onTap: () {
+                  // TODO: Implement onTap functionality for Hostel Booking
+                },
               ),
               ListTile(
                 title: Text('Evaluation'),
                 leading: Icon(Icons.rate_review),
+                onTap: () {
+                  // TODO: Implement onTap functionality for Evaluation
+                },
               ),
               ListTile(
                 title: Text('LMS'),
                 leading: Icon(Icons.computer),
+                onTap: () {
+                  // TODO: Implement onTap functionality for LMS
+                },
               ),
               ListTile(
                 title: Text('Logout'),
                 leading: Icon(Icons.logout),
+                onTap: () {
+                  // TODO: Implement onTap functionality for Logout
+                },
               ),
             ],
           ),
@@ -93,19 +123,31 @@ class HomePage extends StatelessWidget {
             BottomNavigationBarItem(
               icon: Icon(Icons.home),
               label: 'Home',
-              ),
+              onTap: () {
+                // TODO: Implement onTap functionality for Home in BottomNavigationBar
+              },
+            ),
             BottomNavigationBarItem(
               icon: Icon(Icons.calendar_today),
               label: 'Timetable',
+              onTap: () {
+                // TODO: Implement onTap functionality for Timetable in BottomNavigationBar
+              },
             ),
             BottomNavigationBarItem(
               icon: Icon(Icons.notifications),
               label: 'Notifications',
-              ),
+              onTap: () {
+                // TODO: Implement onTap functionality for Notifications in BottomNavigationBar
+              },
+            ),
             BottomNavigationBarItem(
               icon: Icon(Icons.account_circle_sharp),
               label: 'Profile',
-              ),
+              onTap: () {
+                // TODO: Implement onTap functionality for Profile in BottomNavigationBar
+              },
+            ),
           ],
         ),
       ),
