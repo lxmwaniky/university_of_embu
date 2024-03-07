ec3ec958603e44dfb113a74ed9a3c2a03a758ef3
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-03-07 21:02:03 +0300

    Add LoginPage, CustomAppBar, and SearchArea widgets

--- lib/main.dart
+++ lib/main.dart
@@ -19,3 +19,46 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.green[100],
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              SizedBox(height: 20),
+              CustomAppBar(),
+              SizedBox(height: 20),
+              SearchArea(),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class SearchArea extends StatelessWidget {
+  const SearchArea({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
