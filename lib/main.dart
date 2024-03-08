78c081314a72919cb486a206f0319ce0a4e51b9a
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-03-08 17:40:36 +0300

    Remove unnecessary code in SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,6 @@ class SplashScreen extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
       return Scaffold(
-    
         backgroundColor: Colors.green[300],
         body: Center(
           child: Column(
