713866fc374e0f7ffeb5b5ec5893466c1bb4ec30
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-03-08 17:55:33 +0300

    Remove SplashScreen widget

--- lib/main.dart
+++ lib/main.dart
@@ -16,41 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         useMaterial3: true,
       ),
-      home: const SplashScreen(),
+      
     );
   }
 }
-
-class SplashScreen extends StatelessWidget {
-    const SplashScreen({super.key});
-
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        backgroundColor: Colors.green[300],
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset('assets/images/logo.png',
-                width: 200,
-                height: 200,
-              ),
-              const SizedBox(height: 30),
-              const Text(
-                'University of Embu \n\t\tStudents Portal', 
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blue)
-                ),
-              const SizedBox(height: 20),
-              const CircularProgressIndicator(color: Colors.red,),
-              const SizedBox(height: 20),
-              const Text('Loading...'),
-            ],
-          ),
-        ),
-      );
-    }
-  }
