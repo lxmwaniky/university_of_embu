e14d0cfd324f674080b28979b8586ecc6b184281
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-03-08 19:11:08 +0300

    Add logo image to SplashScreen

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -6,15 +6,37 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.green[100],
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            
+            Text('Students Portal',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey[900],
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
             Image.asset(
               'assets/images/logo.png',
               width: 200,
               height: 200,
+              fit: BoxFit.fill,
               ),
+            const SizedBox(height: 20),
+            Text('University of Embu',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey[700],
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            const CircularProgressIndicator(),
           ],
         ),
       ),
