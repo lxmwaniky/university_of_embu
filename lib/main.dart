288160a3cba1b5c3392d2ed43de5d51eb80ed369
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-03-08 08:51:05 +0300

    Update logo asset file extension

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class SplashScreen extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Image.asset('assets/images/logo.jpeg'),
+              Image.asset('assets/images/logo.png'),
               const SizedBox(height: 30),
               const Text(
                 'University of Embu \n\t\tStudents Portal', 
