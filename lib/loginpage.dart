57d3159726b8f9a8271a4275b7ebfd2ec1162e60
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-03-09 10:12:54 +0300

    Update navigation to LoginPage in ForgotPassword class

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -228,7 +228,7 @@ class ForgotPassword extends StatelessWidget {
             {
               Navigator.push(
                 context, MaterialPageRoute(
-                  builder: (context) => const HomePage(),
+                  builder: (context) => const LoginPage(),
                 ),
               );
             }, 
