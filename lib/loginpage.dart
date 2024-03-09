42045233ca5d857281eac7668981519205918203
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-03-09 10:05:53 +0300

    Add registration form to login page

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -107,7 +107,84 @@ class SignUp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.green[100],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Account Registration',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey[900],
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            Image.asset(
+              'assets/images/logo.png',
+              width: 70,
+              fit: BoxFit.fill,
+            ),
+            const SizedBox(height: 20),
+            const SizedBox(
+              width: 330,
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Registration no.',
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            const SizedBox(
+              width: 330,
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            const SizedBox(
+              width: 330,
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Confirm Password',
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(onPressed: ()
+            {
+              Navigator.push(
+                context, MaterialPageRoute(
+                  builder: (context) => const HomePage(),
+                ),
+              );
+            }, 
+            child: const Text(
+              'Sign Up',
+            ),
+            ),
+            TextButton(onPressed: () {
+              Navigator.push(
+                context, MaterialPageRoute(
+                  builder: (context) => const LoginPage(),
+                  ),
+              );
+            }, 
+            child: const Text('Back to Login'),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
 
