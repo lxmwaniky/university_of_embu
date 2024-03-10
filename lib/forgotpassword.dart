a434c6c09bd6425c39fdcea9dcbeb36ec6417868
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-03-10 18:09:35 +0300

    Refactor login page code

---
+++ lib/forgotpassword.dart
@@ -0,0 +1,64 @@
+class ForgotPassword extends StatelessWidget {
+  const ForgotPassword({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green[100],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Recover Password',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey[900],
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            Image.asset(
+              'assets/images/logo.png',
+              width: 70,
+              fit: BoxFit.fill,
+            ),
+            const SizedBox(height: 20),
+            const SizedBox(
+              width: 330,
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Student Email',
+                  hintText: 'Enter your recovery email',
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(onPressed: ()
+            {
+              Navigator.push(
+                context, MaterialPageRoute(
+                  builder: (context) => const LoginPage(),
+                ),
+              );
+            }, 
+            child: const Text(
+              'Reset Password',
+            ),
+            ),
+            TextButton(onPressed: () {
+              Navigator.push(
+                context, MaterialPageRoute(
+                  builder: (context) => const LoginPage(),
+                  ),
+              );
+            }, 
+            child: const Text('Back to Login'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/signuppage.dart
+++ lib/signuppage.dart
@@ -1,24 +1,5 @@
-import 'package:flutter/material.dart';
-
-class LoginButton extends StatelessWidget {
-  const LoginButton({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => const LoginPage()),
-        );
-      },
-      child: const Text('Welcome'),
-    );
-  }
-}
-
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+class SignUp extends StatelessWidget {
+  const SignUp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +9,7 @@ class LoginPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Login to the Portal',
+            Text('Account Registration',
               style: TextStyle(
                 fontSize: 30,
                 fontWeight: FontWeight.bold,
@@ -43,13 +24,31 @@ class LoginPage extends StatelessWidget {
               fit: BoxFit.fill,
             ),
             const SizedBox(height: 20),
-            const SizedBox(
+            SizedBox(
               width: 330,
               child: TextField(
                 decoration: InputDecoration(
-                  border: OutlineInputBorder(),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(13),
+                  ),
                   labelText: 'Student Email',
                   hintText: 'Enter Student Email',
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            SizedBox(
+              width: 330,
+              child: TextField(
+                obscureText: true,
+                enableSuggestions: false,
+                autocorrect: false,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(13),
+                  ),
+                  labelText: 'Password',
+                  hintText: 'Enter your Password',
                 ),
               ),
             ),
@@ -62,8 +61,8 @@ class LoginPage extends StatelessWidget {
                 enableSuggestions: false,
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
-                  labelText: 'Password',
-                  hintText: 'Enter your Password',
+                  labelText: 'Confirm Password',
+                  hintText: 'Confirm your Password',
                 ),
               ),
             ),
@@ -72,50 +71,25 @@ class LoginPage extends StatelessWidget {
             {
               Navigator.push(
                 context, MaterialPageRoute(
-                  builder: (context) => const HomePage(),
+                  builder: (context) => const LoginPage(),
                 ),
               );
             }, 
             child: const Text(
-              'Login',
+              'Sign Up',
             ),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context, MaterialPageRoute(
-                    builder: (context) => const ForgotPassword(),
-                    ),
-                );
-              },
-              child: const Text('Forgot password?'),
             ),
             TextButton(onPressed: () {
               Navigator.push(
                 context, MaterialPageRoute(
-                  builder: (context) => const SignUp(),
+                  builder: (context) => const LoginPage(),
                   ),
               );
             }, 
-            child: const Text('Create an Account'),
+            child: const Text('Back to Login'),
             )
           ],
         ),
-      ),
-    );
-  }
-}
-
-
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('University of Embu'),
       ),
     );
   }
