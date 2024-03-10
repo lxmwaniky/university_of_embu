68e798e17c8d9c189a07cb62e1cc55cfcba9210b
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-03-10 18:11:11 +0300

    Update user authentication logic

---
+++ lib/homepage.dart
@@ -0,0 +1,12 @@
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('University of Embu'),
+      ),
+    );
+  }
+}
