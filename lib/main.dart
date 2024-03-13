4b2d654242439335a6f6d9e73e52338295977180
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-03-14 00:19:22 +0300

    Remove unused imports and dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:university_of_embu/viewpage.dart';
 import 'firebase_options.dart';
-import 'firebase_auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
