0a6c83dc975d47cf88508cb9f59f4e0c549bdb77
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-03-13 17:36:42 +0300

    Refactor HomePageBody constructor to use a required Key parameter and add styling to Today's Lessons

--- lib/homepagebody.dart
+++ lib/homepagebody.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomePageBody extends StatelessWidget {
-  const HomePageBody({super.key});
+  const HomePageBody({required Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -13,8 +13,34 @@ class HomePageBody extends StatelessWidget {
             children: [
               Text(
                 'Today\'s Lessons',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              Table()
+              Table(
+                children: [
+                  TableRow(
+                    children: [
+                      Text('Unit Code'),
+                      Text('Time'),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      Text('CS101'),
+                      Text('9:00 AM'),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      Text('MATH202'),
+                      Text('11:00 AM'),
+                    ],
+                  ),
+                  // Add more unit codes and times here
+                ],
+              ),
             ],
           ),
           // Assignments
