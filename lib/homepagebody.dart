ae92f98c6ec48eefe6c090e9bb474e0b0bdb25af
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-03-13 16:19:21 +0300

    Delete homepagescreen.dart file

---
+++ lib/homepagebody.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class HomePageBody extends StatelessWidget {
+  const HomePageBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Welcome to the University of Embu',
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Colors.grey[900],
+            ),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 20),
+          Image.asset(
+            'assets/images/logo.png',
+            width: 200,
+            height: 200,
+            fit: BoxFit.fill,
+          ),
+          const SizedBox(height: 20),
+          const LoginButton(),
+        ],
+      ),
+    );
+  }
+}
