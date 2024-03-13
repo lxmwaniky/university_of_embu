e0208ed2df1cb3e7dad8916ac1f8f7ef27585dff
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-03-14 00:39:35 +0300

    Refactor MyApp to use StatefulWidget and add TextEditingController

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,16 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
 
   @override
   Widget build(BuildContext context) {
