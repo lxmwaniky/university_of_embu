2423fe1d0676fdf7f0795105c59607ce171064b0
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-03-13 16:12:08 +0300

    Update navigation in LoginPage

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -60,7 +60,7 @@ class LoginPage extends StatelessWidget {
             const SizedBox(height: 20),
             ElevatedButton(onPressed: ()
             {
-              Navigator.push(
+              Navigator.pushReplacement(
                 context, MaterialPageRoute(
                   builder: (context) => const HomePage(),
                 )
@@ -72,7 +72,7 @@ class LoginPage extends StatelessWidget {
             ),
             TextButton(
               onPressed: () {
-                Navigator.push(
+                Navigator.pushReplacement(
                   context, MaterialPageRoute(
                     builder: (context) => const ForgotPassword(),
                     ),
@@ -81,7 +81,7 @@ class LoginPage extends StatelessWidget {
               child: const Text('Forgot password?'),
             ),
             TextButton(onPressed: () {
-              Navigator.push(
+              Navigator.pushReplacement(
                 context, MaterialPageRoute(
                   builder: (context) => const SignUp(),
                   ),
