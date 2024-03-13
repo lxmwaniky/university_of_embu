1a6449a86dc05d1076592ddfefaa1d06826a29e6
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-03-13 10:38:22 +0300

    Update imports and styles in homepage.dart

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -8,7 +10,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.green[100],
       appBar: AppBar(
-        backgroundColor: Colors.green[200],
+        backgroundColor: Colors.green[300],
         title: const Text(
           'University of Embu',
           style: TextStyle(),
@@ -20,8 +22,20 @@ class HomePage extends StatelessWidget {
         child: ListView(
           children: [
             UserAccountsDrawerHeader(
-              accountName: const Text('Alex Mwaniki Nyambura'),
-              accountEmail: const Text('27465@student.embuni.ac.ke'),
+              accountName: const Text(
+                'Alex Mwaniki Nyambura',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.black,
+                  ),
+                ),
+              accountEmail: const Text(
+                '27465@student.embuni.ac.ke',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.black,
+                  )
+                ),
               currentAccountPicture: const CircleAvatar(
                 backgroundImage: AssetImage(
                   'assets/images/alex.png',
