94f222ec9d19e3dac36eef3e3815496596528d73
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-03-20 09:25:14 +0300

    Reduce splash screen delay to 3 seconds

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -11,7 +11,7 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
-    Future.delayed(Duration(seconds: 5), () {
+    Future.delayed(Duration(seconds: 3), () {
       Navigator.pushAndRemoveUntil(
           context,
           MaterialPageRoute(
