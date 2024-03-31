babce8be08e63a39d69474b0a09284ead0c0f400
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-03-31 21:55:32 +0300

    Add delayed navigation to LoginPage in IntroPage

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class IntroPage extends StatelessWidget {
+class IntroPage extends StatefulWidget {
   const IntroPage({super.key});
+
+  @override
+  State<IntroPage> createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> {
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginPage(),),);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
