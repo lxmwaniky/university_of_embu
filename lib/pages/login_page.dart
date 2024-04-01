24fcac11250358fe7fa06ff7c91f2fe02cda2982
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-04-01 23:59:55 +0300

    Add sign-in functionality with School Email

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatelessWidget {
@@ -30,18 +31,40 @@ class LoginPage extends StatelessWidget {
             const SizedBox(
               height: 20,
             ),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset('./lib/assets/images/google-logo.png', height: 25,),
-                  const SizedBox(width: 10,),
-                  Text('Sign in with School Email', style: GoogleFonts.roboto(fontSize: 15, fontWeight: FontWeight.bold),),
-                ],
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const HomePage(),
+                  ),
+                );
+              },
+              child: Container(
+                width: 300,
+                decoration: BoxDecoration(
+                  color: Colors.green.shade400,
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                padding: EdgeInsets.all(5),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      './lib/assets/images/google-logo.png',
+                      height: 25,
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      'Sign in with School Email',
+                      style: GoogleFonts.roboto(
+                          fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
