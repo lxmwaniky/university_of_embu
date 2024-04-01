887ed23b757aeddee29eb7d84c354a8a56cc302c
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-04-01 23:51:53 +0300

    Refactor LoginPage design and add sign-in button with school email

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatelessWidget {
-  LoginPage({super.key});
+  const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -13,10 +13,11 @@ class LoginPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              'LOGIN',
-              style: GoogleFonts.aboreto(
+              'Login',
+              style: GoogleFonts.roboto(
+                fontSize: 27,
                 fontWeight: FontWeight.bold,
-                fontSize: 25,
+                color: Colors.grey[700],
               ),
             ),
             const SizedBox(
@@ -24,31 +25,25 @@ class LoginPage extends StatelessWidget {
             ),
             Image.asset(
               './lib/assets/images/University-of-Embu-logo.png',
-              height: 170,
+              height: 150,
             ),
             const SizedBox(
               height: 20,
             ),
             Container(
               decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.teal,
-                ),
-                borderRadius: BorderRadius.circular(12.0),
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
               ),
-              padding: EdgeInsets.all(12.0),
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Image.asset('./lib/assets/images/google-logo.png',
-                      height: 20,),
-                  Text(
-                    'Login with Student Email',
-                    style: GoogleFonts.aboreto(
-                        fontWeight: FontWeight.bold, fontSize: 10),
-                  ),
+                  Image.asset('./lib/assets/images/google-logo.png', height: 25,),
+                  const SizedBox(width: 10,),
+                  Text('Sign in with School Email', style: GoogleFonts.roboto(fontSize: 15, fontWeight: FontWeight.bold),),
                 ],
               ),
-            )
+            ),
           ],
         ),
       ),
