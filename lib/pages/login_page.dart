41a3845320e2701bff8b9483a62a261822ebccae
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-04-01 23:23:37 +0300

    Add login page UI enhancements

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatelessWidget {
   LoginPage({super.key});
@@ -11,7 +12,21 @@ class LoginPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('LOGIN'),
+            Text(
+              'LOGIN',
+              style:
+                  GoogleFonts.roboto(fontWeight: FontWeight.bold, fontSize: 25),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Image.asset(
+              './lib/assets/images/University-of-Embu-logo.png',
+              height: 170,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
           ],
         ),
       ),
