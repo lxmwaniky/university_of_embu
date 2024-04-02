ce93b1b59a2205416244c06a9933b7e0d7e92944
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-04-03 01:58:03 +0300

    Refactor signInWithGoogle method to handle null GoogleSignInAccount

--- lib/auth/auth_services.dart
+++ lib/auth/auth_services.dart
@@ -2,17 +2,25 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
-  Future <UserCredential> signInWithGoogle() async {
-    // Interactive Process
-    final GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
-    // Obtain Details
-    final GoogleSignInAuthentication gAuth = await gUser!.authentication;
-    // Create new credential
-    final credential = GoogleAuthProvider.credential(
-      accessToken: gAuth.accessToken,
-      idToken: gAuth.idToken,
-    );
-    // Use credential to sign in
-    return await FirebaseAuth.instance.signInWithCredential(credential);
+  Future<UserCredential?> signInWithGoogle() async {
+    try {
+      // Interactive Process
+      final GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
+      if (gUser == null) {
+        return null;
+      }
+      // Obtain Details
+      final GoogleSignInAuthentication gAuth = await gUser.authentication;
+      // Create new credential
+      final credential = GoogleAuthProvider.credential(
+        accessToken: gAuth.accessToken,
+        idToken: gAuth.idToken,
+      );
+      // Use credential to sign in
+      return await FirebaseAuth.instance.signInWithCredential(credential);
+    } catch (e) {
+      print(e);
+      return null;
+    }
   }
 }
