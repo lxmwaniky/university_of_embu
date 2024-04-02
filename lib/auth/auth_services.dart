61b2cc05b065b959cf527238ab1516b8ec14b29f
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-04-03 01:13:57 +0300

    add(auth)

--- lib/auth/auth_services.dart
+++ lib/auth/auth_services.dart
@@ -1,11 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
 class AuthService {
   signInWithGoogle() async {
     // Interactive Process
-
+    final GoogleSignInAccount? user = await GoogleSignIn().signIn();
     // Obtain Details
-
+    final GoogleSignInAuthentication auth = await user!.authentication;
     // Create new credential
-
+    final credential = GoogleAuthProvider.credential(
+      accessToken: auth.accessToken,
+      idToken: auth.idToken,
+    );
     // Use credential to sign in
+    return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 }
