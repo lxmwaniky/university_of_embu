c9c7f0584997d8c50d8c4c96de659ac9339b2b3f
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-04-05 21:37:54 +0300

    Update login and home pages with Firebase authentication and secure storage integration

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,16 +1,47 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:university_of_embu/pages/login_page.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser;
+    final storage = const FlutterSecureStorage();
+
+
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Comrade Portal'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+              await storage.delete(key: 'your_token_key');
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => LoginPage(),
+                ),
+              );
+            },
+            icon: Icon(Icons.logout),
+          ),
+        ],
+      ),
       backgroundColor: Colors.lightBlue,
-      body: Center(
-          child: Text(
-        'You are now logged in!',
-      )),
+      body: Container(
+        child: user != null
+            ? Text(
+                'Welcome ${user.displayName}',
+              )
+            : const Text(
+                'Welcome, Comrade',
+              ),
+      ),
     );
   }
 }
