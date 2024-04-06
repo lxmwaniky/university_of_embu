4ec44a9a062d0339a10bf4ec8a5ba02f00056300
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-04-06 21:13:03 +0300

    Refactor signOutAndClearCache method in auth_services.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:university_of_embu/auth/auth_services.dart';
-import 'package:university_of_embu/pages/login_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
