6c41a9024a45f410668354fe6c57be061b5c50ab
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-04-08 00:43:11 +0300

    Remove unused import in home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:university_of_embu/widgets/info_section.dart';
 import 'package:university_of_embu/widgets/navigation_drawer.dart';
